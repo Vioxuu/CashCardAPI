240e6b8d916a5621d0b7c806c6ba11ef2a777a1e
Bartek <vbbarteq@gmail.com>
2026-02-23 13:00:59 +0100

    Added @Id annotation to CashCard; Changed findById to use repo

--- src/main/java/com/example/cashcard/CashCardController.java
+++ src/main/java/com/example/cashcard/CashCardController.java
@@ -6,17 +6,24 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/cashcards")
 public class CashCardController {
 
+    private final CashCardRepository cashCardRepository;
+
+    private CashCardController(CashCardRepository cashCardRepository) {
+        this.cashCardRepository = cashCardRepository;
+    }
+
     @GetMapping("/{id}")
     private ResponseEntity<CashCard> findById(@PathVariable Long id) {
-        if(id.equals(99L)) {
-            CashCard cashCard = new CashCard(99L, 123.45);
-            return ResponseEntity.ok(cashCard);
-        }else{
+        Optional<CashCard> cashcardOptional = cashCardRepository.findById(id);
+        if(cashcardOptional.isPresent())
+            return ResponseEntity.ok(cashcardOptional.get());
+        else
             return ResponseEntity.notFound().build();
-        }
     }
 }
